32df31e9258a628bd025ce15aa1b110888ee56fc
José Sánchez <jose.venados6@gmail.com>
2026-04-07 12:05:37 -0600

    fix: Duplication in api path

--- backend/urls.py
+++ backend/urls.py
@@ -5,11 +5,13 @@ admin.site.site_header = 'UMES Administration'
 admin.site.site_title = 'UMES Admin'
 admin.site.index_title = 'University Management System'
 
+API_PATH='api/v1/'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('apps.academics.urls')),
-    path('api/v1/', include('apps.students.urls')),
-    path('api/v1/', include('apps.staff.urls')),
-    path('api/v1/', include('apps.classes.urls')),
-    path('api/v1/', include('apps.enrollments.urls')),
+    path(API_PATH, include('apps.academics.urls')),
+    path(API_PATH, include('apps.students.urls')),
+    path(API_PATH, include('apps.staff.urls')),
+    path(API_PATH, include('apps.classes.urls')),
+    path(API_PATH, include('apps.enrollments.urls')),
 ]
